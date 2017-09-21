24cda993f36c5fc11dbb82fc38c7b39dbdd20f73
ckh8599 <ckh8599@hanmail.net>
2017-09-22 08:46:39 +0900

    barcode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-// import { PopoverController } from 'ionic-angular';
 import { ModalController, NavParams } from 'ionic-angular';
 import { CodePage } from '../code/code';
 
